22374b803b60acfede66758463355041c4ec2969
rubina <rubina.digitaly@gmail.com>
2025-10-09 13:14:59 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p className="read-the-docs text-xl">
         Click on the Vite and React logos to learn more
       </p>
     </div>
